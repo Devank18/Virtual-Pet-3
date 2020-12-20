935c8e2b15e34e857e58d8bb2437d94492f4f836
Devank18 <67155685+Devank18@users.noreply.github.com>
2020-12-20 09:55:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,30 @@
-//Create variables here
 var dog, happyDog, database, foodS, foodStock,bg , FeedDog , FeedDog,bed,wc,garden;
 var fedTime, lastFed ;
 var foodObj;
 var dogImage;
 var gst,gsread;
 
-function preload()
-{
-	//load images here
-
+function preload(){
   dogImage=loadImage("images/dogImg.png");
   happyDog=loadImage("images/dogImg1.png");
   bg=loadImage("images/room.jpg");
   bed = loadImage("pics/Bed Room.png")
   wc = loadImage("pics/Wash Room.png")
   garden = loadImage("pics/Garden.png");
-
 }
 
-function setup() {
-
+function setup(){
   database=firebase.database();
 	createCanvas(1000, 500);
   
-
-
-  foodObj=new Food();
-
+	foodObj=new Food();
    fill("blue");
+	
   FeedDog=createButton("FeedDog");
   FeedDog.position(1200,50);
   
    AddMilk=createButton("AddMilk");
   AddMilk.position(1200,95);
-  
 
   gsread=database.ref('gameState');
   gsread.on("value",function (data){
@@ -41,31 +32,21 @@ function setup() {
 gameState=data.val();
 
   });
-
   dog=createSprite(750,400,150,150);
   dog.addImage(dogImage);
   dog.scale=0.15;
-
-  
 }
-
-
-function draw() {  
+function draw(){  
 background(bg);
-
+	
 foodS=foodObj.getFoodStock();
-
-
- 
- AddMilk.mousePressed(()=>{
-
-  foodS++;
-  database.ref('/').update({
-    Food:foodS
+AddMilk.mousePressed(()=>{
+foodS++;
+database.ref('/').update({
+Food:foodS
   })
     
 });
-
 FeedDog.mousePressed(()=>{
   dog.addImage(happyDog);
 
@@ -81,10 +62,7 @@ FeedDog.mousePressed(()=>{
   fedTime.on("value",function(data){
     lastFed=data.val();
   });
-
-
-   currentTime=hour();
-
+currentTime=hour()
               if(currentTime==(lastFed+1)){
                         update("playing");
                         foodObj.garden();
@@ -99,15 +77,6 @@ FeedDog.mousePressed(()=>{
                           update("hungry")
                           foodObj.display();
                  }
-
-
-
-
-
-
-
-
-
    fill("black");
   textSize(15);
   if(lastFed>=12){
@@ -117,33 +86,20 @@ FeedDog.mousePressed(()=>{
    }else{
      text("Last Feed : "+ lastFed + " AM", 200,30);
    }
-
-
  if(gameState!=="hungry"){
    FeedDog.hide();
    AddMilk.hide();
    dog.visible=false ;
-
-
- }else{
-
+ }
+else{
   FeedDog.show();
   AddMilk.show();
   dog.addImage(dogImage);
  }
-
-
-
-
-
-
 foodStock=database.ref('Food');
 foodStock.on("value",readStock);
-
-
+	
   drawSprites();
-
-  //add styles here
   fill("red");
   stroke("red");
   strokeWeight(1);
@@ -155,32 +111,24 @@ foodStock.on("value",readStock);
 function readStock(data){
   foodStock=data.val();
   foodObj.updateFoodStock(foodS);
-  
 }
 
 function writeStock(x){
-
  if(x<=0){
    x=0
  }else{
    x-=0.25;
  }
 
-
-  database.ref('/').update({
+	database.ref('/').update({
     Food:x
-  })
-
-  
+  }) 
 }
 
 function update(state) {
-
   database.ref('/').update({
     gameState:state
   });
-
-
 }
 
 
